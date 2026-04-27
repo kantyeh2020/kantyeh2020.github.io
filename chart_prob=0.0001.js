cbe2126da7a11ae3f869e78278c800103766e874
葉貫中 <kantyeh2020@gapp.nthu.edu.tw>
2026-04-28 00:36:34 +0800

    Update chart_prob=0.0001.js

--- chart_prob=0.0001.js
+++ chart_prob=0.0001.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
 
         subtitle: {
-            text: "Generated automatically from expressibility_transform_to_parameters_mean_std_circuit*_countsall.csv",
+            text: "Generated automatically from expressibility_transform_to_parameters_mean_std_circuit*_prob0.000100_countsall.csv",
             align: "left"
         },
 
